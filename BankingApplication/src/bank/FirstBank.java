c435e6976a0fe7e92b8ce48ba580b716d567001d
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-06-24 23:17:31 +0530

    Project

---
+++ BankingApplication/src/bank/FirstBank.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account Arsh = new Account("Babayaga","A2003");
+		Arsh.showMenu();
+	}
+
+}
